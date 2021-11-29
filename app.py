8be358770bdf69b3256b8866312e5bb994aa6714
Charles Leclerc <chl.charlesleclerc@gmail.com>
2021-11-29 12:33:46 +0100

    image implementation

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 
 import streamlit as st
 
-import datetime
+import streamlit as st
+from PIL import Image
 
-import requests
 
 '''
 # Neural Style Transfer front
 
 This is a test for Neural Style Transfer
+
 '''
+
+
+
+st.title("Upload + Classification Example")
+
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+image = Image.open(uploaded_file)
+st.image(image, caption='Uploaded Image.', use_column_width=True)
+st.write("")
+st.write("Classifying...")
